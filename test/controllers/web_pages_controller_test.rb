5908c3caf78fd2501d9548a7e5e08c31a4863019
Magni Hansen <magni.mh@gmail.com>
2018-04-04 21:59:10 +0000

    WebPage Title Tests

--- test/controllers/web_pages_controller_test.rb
+++ test/controllers/web_pages_controller_test.rb
@@ -1,28 +1,39 @@
 require 'test_helper'
 
 class WebPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Sunset Vacations" 
+  end
+  
   test "should get home" do
     get web_pages_home_url
     assert_response :success
+    assert_select "title", "#{@base_title} >> Home"
   end
 
   test "should get about" do
     get web_pages_about_url
     assert_response :success
+    assert_select "title", "#{@base_title} >> About"
   end
   
   test "should get cities" do
     get web_pages_cities_url
     assert_response :success
+    assert_select "title", "#{@base_title} >> Cities"
   end
   
   test "should get toptens" do
     get web_pages_toptens_url
     assert_response :success
+    assert_select "title", "#{@base_title} >> Top Tens"
   end
 
   test "should get packages" do
     get web_pages_packages_url
     assert_response :success
+    assert_select "title", "#{@base_title} >> Packages"
   end
 end
+
